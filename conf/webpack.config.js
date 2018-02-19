5ce7f1d155e5f86ae76468bd2cc361382048a840
Markus Opolka <markus@martialblog.de>
2018-02-19 10:05:08 +0100

    Add Webpack/Babel configuration

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,3 +1,20 @@
-module.exports = function(env) {
-  return require(`./conf/webpack.${env}.js`)
+const path = require('path');
+const webpack = require('webpack');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
+module.exports = {
+  entry: {
+    'ceql': './src/index.js',
+    'ceql.min': './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+    libraryTarget: 'window'
+  },
+  plugins: [
+    new MinifyPlugin({},{
+      test: /\.min\.js($|\?)/
+    })
+  ]
 }
